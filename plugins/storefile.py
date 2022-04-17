fc35cf2f30855c3ef8c1cd8381d67044cb218f8e
P̤radeep <78857587+Pradeep-iKp@users.noreply.github.com>
2022-04-17 15:43:38 +0530

    Update storefile.py

--- plugins/storefile.py
+++ plugins/storefile.py
@@ -6,18 +6,18 @@ from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 from config import *
 
 
-@Client.on_message(filters.command(['link', 'store']) & ~filters.edited)
-async def storefile(bot, message):
-    replied = message.reply_to_message
-    if not replied:
-        return await message.reply('hmm')
-    file_type = replied.media
-    if file_type not in ["photo", "video", 'audio', 'document']:
-        return await message.reply('Hm')
-    bot = await c.get_me()
-    base64_string = await encode_string(f"{m.chat.id}_{msg.message_id}")
-    url = f"https://t.me/{bot.username}?start={base64_string}"
-    await message.reply(f"Here is Link : {url}")
+# @Client.on_message(filters.command(['link', 'store']) & ~filters.edited)
+# def storefile(bot, message):
+  #  replied = message.reply_to_message
+  #  if not replied:
+   #     return await message.reply('hmm')
+   # file_type = replied.media
+  #  if file_type not in ["photo", "video", 'audio', 'document']:
+   #     return await message.reply('Hm')
+   # bot = await c.get_me()
+   # base64_string = await encode_string(f"{m.chat.id}_{msg.message_id}")
+  #  url = f"https://t.me/{bot.username}?start={base64_string}"
+   # await message.reply(f"Here is Link : {url}")
     
     
 
