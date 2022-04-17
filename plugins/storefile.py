1419b709f32dcb76ff654e9382f96877c8f52ef3
P̤radeep <78857587+Pradeep-iKp@users.noreply.github.com>
2022-04-17 14:46:42 +0530

    Update storefile.py

--- plugins/storefile.py
+++ plugins/storefile.py
@@ -5,6 +5,28 @@ from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 from config import *
 
+
+@Client.on_message(filter.command(['link', 'store']) & ~filters.edited)
+async def storefile(bot, message):
+    replied = message.reply_to_message
+    if not replied:
+        return await message.reply('hmm')
+    file_type = replied.media
+    if file_type not in ["photo", "video", 'audio', 'document']:
+        return await message.reply('Hm')
+    bot = await c.get_me()
+    base64_string = await encode_string(f"{m.chat.id}_{msg.message_id}")
+    url = f"https://t.me/{bot.username}?start={base64_string}"
+    await message.reply(f"Here is Link : {url}")
+    
+    
+
+
+
+
+
+
+
 #################################### FOR PRIVATE ################################################
 @Client.on_message((filters.document|filters.video|filters.audio|filters.photo) & filters.incoming & ~filters.edited & ~filters.channel)
 async def storefile(c, m):
